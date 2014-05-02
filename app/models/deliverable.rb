c71ad748cac89493baa395c7b2a92d993e28944e
nelson pollard <nelson.pollard@sv.cmu.edu>
2014-05-01 19:08:46 -0700

    Nil check for student_id in get_status_for_every_individual

--- app/models/deliverable.rb
+++ app/models/deliverable.rb
@@ -414,14 +414,16 @@ class Deliverable < ActiveRecord::Base
   #Todo: rename get_status_for_every_individual to status_for_every_individual
   # To get the status of deliverable by student for is it graded or not.
   def get_status_for_every_individual(student_id)
-    return :unknonwn if self.assignment.nil? #(guard for old deliverables)
-    grade = Grade.get_grade(self.course.id, self.assignment.id, student_id)
-    if grade.nil?
-      return :ungraded
-    elsif !grade.is_student_visible?
-      return :drafted
-    else
-      return :graded
+    unless (student_id.nil?)
+      return :unknonwn if self.assignment.nil? #(guard for old deliverables)
+      grade = Grade.get_grade(self.course.id, self.assignment.id, student_id)
+      if grade.nil?
+        return :ungraded
+      elsif !grade.is_student_visible?
+        return :drafted
+      else
+        return :graded
+      end
     end
   end
 
